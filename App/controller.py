1f08b569dd84c03e9ff58510cf1acee6b81e5584
AndresVilla203 <69772391+AndresVilla203@users.noreply.github.com>
2020-11-26 17:26:17 -0500

    Update controller.py

--- App/controller.py
+++ App/controller.py
@@ -1,51 +1,55 @@
-"""
- * Copyright 2020, Departamento de sistemas y Computación
- * Universidad de Los Andes
- *
- *
- * Desarrolado para el curso ISIS1225 - Estructuras de Datos y Algoritmos
- *
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- * Contribución de:
- *
- * Dario Correal
- *
- """
-
 import config as cf
 from App import model
 import csv
-
-"""
-El controlador se encarga de mediar entre la vista y el modelo.
-Existen algunas operaciones en las que se necesita invocar
-el modelo varias veces o integrar varias de las respuestas
-del modelo en una sola respuesta.  Esta responsabilidad
-recae sobre el controlador.
-"""
-
+import os
 # ___________________________________________________
 #  Inicializacion del catalogo
+def InitCatalog():
+    Analyzer = model.analyzer()
+    return Analyzer
 # ___________________________________________________
 
 
 # ___________________________________________________
 #  Funciones para la carga de datos y almacenamiento
 #  de datos en los modelos
+def loadTrips(analyzer, filename):
+    archivos = []
+    print(filename)
+    cargar = loadFile(analyzer, filename)
+    Arcos = model.TotalDeArcos(analyzer)
+    Vertices = model.TotalDeVertices(analyzer)
+    Clusters = model.TotaldeClusteres(analyzer)
+    archivos.append({"Total de Vertices":Vertices})
+    archivos.append({"Total de Arcos":Arcos})
+    archivos.append({"Total de Clusters":Clusters})
+    return archivos
+def loadFile(analyzer, file):
+    """
+    """
+    file = cf.data_dir + file
+    input_file = csv.DictReader(open(file, encoding="utf-8"),
+                                delimiter=",")
+    for route in input_file:
+        model.AñadirRuta(analyzer, route)
+    return analyzer
+
 # ___________________________________________________
 
 # ___________________________________________________
 #  Funciones para consultas
 # ___________________________________________________
+def totalClusters(analyzer):
+    retorno = model.TotaldeClusteres(analyzer)
+    return retorno 
+def totalarcos(analyzer):
+    retorno = model.TotalDeArcos(analyzer)
+    return retorno
+def totalvertices(analyzer):
+    retorno = model.TotalDeVertices(analyzer)
+    return retorno 
+def clusterentredosID(analyzer,id1,id2):
+    retorno = model.ClusterPresence(analyzer,id1,id2)
+    return 
+def ensayo(analyzer):
+    return model.ensayo(analyzer)
