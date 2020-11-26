a12508f2f48d9dbf385c36b05981e20693be01d5
AndresVilla203 <69772391+AndresVilla203@users.noreply.github.com>
2020-11-26 17:24:37 -0500

    Update view.py

--- App/view.py
+++ App/view.py
@@ -1,30 +1,3 @@
-"""
- * Copyright 2020, Departamento de sistemas y Computación
- * Universidad de Los Andes
- *
- *
- * Desarrolado para el curso ISIS1225 - Estructuras de Datos y Algoritmos
- *
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- * Contribución de:
- *
- * Dario Correal
- *
- """
-
-
 import sys
 import config
 from App import controller
@@ -32,22 +5,49 @@ from DISClib.ADT import stack
 import timeit
 assert config
 
-"""
-La vista se encarga de la interacción con el usuario.
-Presenta el menu de opciones  y  por cada seleccion
-hace la solicitud al controlador para ejecutar la
-operación seleccionada.
-"""
-
-# ___________________________________________________
-#  Variables
-# ___________________________________________________
-
-
 # ___________________________________________________
 #  Menu principal
-# ___________________________________________________
+def Menu():
+    print("\n")
+    print("Bienvenido")
+    print("1- Iniciar el catalogo")
+    print("2- Cargar archivos al catalogo")
+    print("3- Cantidad de clusters en el grafo")
+    print("0- Salir")
 
-"""
-Menu principal
-"""
+# ___________________________________________________
+archivo = "201801-1-citibike-tripdata.csv"
+def option_three(analyzer):
+    id1 = input("ID de la primera estación: ")
+    id2 = input("ID de la segunda estacion: ")
+    total = controller.totalClusters(analyzer)
+    haycluster = controller.clusterentredosID(analyzer,id1,id2)
+    print("Total clústeres: ",total)
+    if haycluster==True:
+        print("Si hay componentes fuertemente conectados entre la id: "+id1+" y la id: "+id2)
+        print(analyzer["grafo"])
+    else:
+        print("No hay componentes fuertemente conectados entre las ids")
+def OpcionesMenu():
+    analyzer = None
+    i = True
+    while i == True:
+        Menu()
+        opciones = str(input("Seleccione una opción:"))
+        if opciones == "1":
+            analyzer = controller.InitCatalog()
+            if analyzer != None:  
+                print("Catalogo creado") 
+            else:
+                print("Error al cargar el catalogo")
+        elif opciones == "2":
+            Datos = controller.loadTrips(analyzer, archivo)
+            print("\n") 
+            print(Datos[0])
+            print(Datos[1])
+            print(Datos[2])
+        elif opciones == "3":
+            option_three(analyzer)
+        elif opciones == "0":
+            i = False
+OpcionesMenu()
